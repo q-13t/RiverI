6776c579817d0ade86c1fb9001b3c20837a2e6e2
q-13t <vova29092002@gmail.com>
2024-02-19 11:08:10 +0200

    RM: docking function

--- src/cpps/ColorEditor.cpp
+++ src/cpps/ColorEditor.cpp
@@ -4,7 +4,6 @@
 void RiverI::ColorEditor::Render(bool * V_Open){
     ImGuiWindowFlags window_flags = 0;
 
-    // Main body of the Demo window starts here.
     if (!ImGui::Begin("Color Editor", V_Open, window_flags)){
         ImGui::End();
         return;
--- src/cpps/Main.cpp
+++ src/cpps/Main.cpp
@@ -54,103 +54,6 @@ static void glfw_error_callback(int error, const char* description) {
 
 
 
-static void useDocking(bool* dock) {
-	// READ THIS !!!
-		// TL;DR; this demo is more complicated than what most users you would normally use.
-		// If we remove all options we are showcasing, this demo would become:
-		//     void ShowExampleAppDockSpace()
-		//     {
-		//         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-		//     }
-		// In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
-		// In this specific demo, we are not using DockSpaceOverViewport() because:
-		// - (1) we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
-		// - (2) we allow the host window to have padding (when opt_padding == true)
-		// - (3) we expose many flags and need a way to have them visible.
-		// - (4) we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport()
-		//      in your code, but we don't here because we allow the window to be floating)
-
-	static bool opt_fullscreen = true;
-	static bool opt_padding = false;
-	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-	// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-	// because it would be confusing to have two docking targets within each others.
-	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-	if (opt_fullscreen)
-	{
-		const ImGuiViewport* viewport = ImGui::GetMainViewport();
-		ImGui::SetNextWindowPos(viewport->WorkPos);
-		ImGui::SetNextWindowSize(viewport->WorkSize);
-		ImGui::SetNextWindowViewport(viewport->ID);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-	}
-	else
-	{
-		dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-	}
-
-	// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-	// and handle the pass-thru hole, so we ask Begin() to not render a background.
-	if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-		window_flags |= ImGuiWindowFlags_NoBackground;
-
-	// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-	// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-	// all active windows docked into it will lose their parent and become undocked.
-	// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-	// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-	if (!opt_padding)
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-	ImGui::Begin("DockSpace Demo", dock, window_flags);
-	if (!opt_padding)
-		ImGui::PopStyleVar();
-
-	if (opt_fullscreen)
-		ImGui::PopStyleVar(2);
-
-	// Submit the DockSpace
-	ImGuiIO& io = ImGui::GetIO();
-	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-	{
-		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-	}
-
-
-	if (ImGui::BeginMenuBar())
-	{
-		if (ImGui::BeginMenu("Options"))
-		{
-			// Disabling fullscreen would allow the window to be moved to the front of other windows,
-			// which we can't undo at the moment without finer window depth/z control.
-			ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
-			ImGui::MenuItem("Padding", NULL, &opt_padding);
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Flag: NoDockingOverCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingOverCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingOverCentralNode; }
-			if (ImGui::MenuItem("Flag: NoDockingSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingSplit; }
-			if (ImGui::MenuItem("Flag: NoUndocking", "", (dockspace_flags & ImGuiDockNodeFlags_NoUndocking) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoUndocking; }
-			if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
-			if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
-			if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Close", NULL, false, dock != NULL))
-				*dock = false;
-			ImGui::EndMenu();
-		}
-
-
-		ImGui::EndMenuBar();
-	}
-
-	ImGui::End();
-}
-
 // Main code
 int main(int, char**) {
 	glfwSetErrorCallback(glfw_error_callback);
@@ -220,7 +123,8 @@ int main(int, char**) {
 
 	// States
 	static bool show_color_editor = true;
-	static bool docking = false;
+	static bool b_color_editor = true;
+	static bool docking = true;
 
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
@@ -239,32 +143,23 @@ int main(int, char**) {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-
-
 		if (docking) ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
 
 		//Top Menu
 		if (ImGui::BeginMainMenuBar()) {
 			if (ImGui::BeginMenu("Options")) {
-
-				if (ImGui::MenuItem("Color Editor", NULL, true)) {}
+				if (ImGui::MenuItem("Color Editor", NULL, &b_color_editor)) {}
 				if (ImGui::MenuItem("Preview Panel", NULL, true)) {}
 				if (ImGui::MenuItem("File Saver", NULL, true)) {}
-				if (ImGui::MenuItem("Dock", NULL, &docking)) {
-					LOG("Docking", docking);
-				};
-				if (ImGui::MenuItem("VSync", NULL, (vsync == 1 ? true : false))) {
-					vsync = (vsync == 1 ? 0 : 1);
-					LOG("Vsync",vsync);
-				};
-
+				if (ImGui::MenuItem("Docking", NULL, &docking)) LOG("Docking", docking);
+				if (ImGui::MenuItem("VSync", NULL, (vsync == 1 ? true : false))) {vsync = (vsync == 1 ? 0 : 1);LOG("Vsync",vsync);};
 				ImGui::EndMenu();
 			}
 			ImGui::EndMainMenuBar();
 		}
-		//Main Part Of Displaying
-		RiverI::ColorEditor::Render(&show_color_editor);
+		
+		if (b_color_editor) RiverI::ColorEditor::Render(&show_color_editor);
 
 
 		//ImGui::ShowDemoWindow();
@@ -277,7 +172,6 @@ int main(int, char**) {
 		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
 		// Update and Render additional Platform Windows
 		// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
 		//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
@@ -288,7 +182,6 @@ int main(int, char**) {
 			ImGui::RenderPlatformWindowsDefault();
 			glfwMakeContextCurrent(backup_current_context);
 		}
-
 		glfwSwapBuffers(window);
 	}
 #ifdef __EMSCRIPTEN__
